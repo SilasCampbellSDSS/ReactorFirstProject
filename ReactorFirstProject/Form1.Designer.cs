9466c282549cd0843b7e80940ba4f2d040da30e8
silacamp873 <silacamp873@SDS-L-9982Y93>
2024-02-09 14:04:09 -0500

    Add project files.

---
+++ ReactorFirstProject/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ReactorFirstProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ReactorI = new System.Windows.Forms.FlowLayoutPanel();
+            this.ReactorII = new System.Windows.Forms.FlowLayoutPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(285, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REACTOR CONTROL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(217, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 31);
+            this.label4.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "REACTOR I";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(578, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "REACTOR II";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // ReactorI
+            // 
+            this.ReactorI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ReactorI.Location = new System.Drawing.Point(122, 264);
+            this.ReactorI.Name = "ReactorI";
+            this.ReactorI.Size = new System.Drawing.Size(200, 100);
+            this.ReactorI.TabIndex = 4;
+            this.ReactorI.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // ReactorII
+            // 
+            this.ReactorII.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ReactorII.Location = new System.Drawing.Point(547, 264);
+            this.ReactorII.Name = "ReactorII";
+            this.ReactorII.Size = new System.Drawing.Size(200, 100);
+            this.ReactorII.TabIndex = 5;
+            this.ReactorII.Paint += new System.Windows.Forms.PaintEventHandler(this.ReactorII_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Gray;
+            this.label5.Font = new System.Drawing.Font("Cascadia Code", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(368, 426);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 30);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "  STABLE  ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(333, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 31);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "OVERIDE REACTOR";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(448, 353);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 31);
+            this.label7.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(823, 488);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ReactorII);
+            this.Controls.Add(this.ReactorI);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Opus Chords Sans Std", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel ReactorI;
+        private System.Windows.Forms.FlowLayoutPanel ReactorII;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ReactorFirstProject/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;//allows the use of Thread.Sleep()
+using System.Media; //allows the use of SoundPlayer
+
+namespace ReactorFirstProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            ReactorI.BackColor = Color.Red
+                ;   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            //create a sound player and load the alert.wav sound, then play it
+            SoundPlayer alertplayer = new SoundPlayer(Properties.Resources.alert);
+            alertplayer.Play();
+            
+            //change the color of label5
+            label5.BackColor = Color.Red;
+
+            //change the message in label5
+            label5.Text = "MELTDOWN!!!";
+
+            //change the message in label6
+            label6.Text = "WHAT DID YOU DO?!?";
+            
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor= Color.Red; 
+            label3.BackColor= Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor= Color.White;
+            ReactorII.BackColor= Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor= Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.Red;
+            ReactorII.BackColor = Color.Red;
+            label5.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            ReactorI.BackColor = Color.White;
+            ReactorII.BackColor = Color.White;
+            label5.BackColor = Color.Red;
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+        }
+
+        private void ReactorII_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
